aa5934a2ee30a5f9248ed556030ec6fa01836893
Luciano <luciano.tonlorenzi@mundi.io>
2023-08-29 11:56:46 -0300

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <section className="bg-gray-100 py-8">
+      <div className="container mx-auto">
+        <h2 className="text-2xl font-semibold mb-4">About Me</h2>
+        <p className="text-gray-700">
+          Hi there! I'm a passionate React developer with a love for creating
+          interactive and engaging web applications. My journey in web
+          development started during my studies, and I've been hooked ever
+          since. I enjoy bringing designs to life using the latest web
+          technologies. When I'm not coding, you'll find me exploring new tech
+          trends and trying out different coding challenges.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
